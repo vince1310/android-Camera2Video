2e9cc71e27ba0817546a351f9a901038d04f27c6
unknown <christopher.w.vincent@gmail.com>
2018-04-12 21:46:58 -0500

    Added Toolbar with menu

--- Application/src/main/java/com/example/android/camera2video/CameraActivity.java
+++ Application/src/main/java/com/example/android/camera2video/CameraActivity.java
@@ -18,13 +18,18 @@ package com.example.android.camera2video;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
-public class CameraActivity extends Activity {
+public class CameraActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
         if (null == savedInstanceState) {
             getFragmentManager().beginTransaction()
                     .replace(R.id.container, Camera2VideoFragment.newInstance())
@@ -32,4 +37,9 @@ public class CameraActivity extends Activity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
 }
